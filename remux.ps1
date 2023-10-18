d03b976031f1711abffc5efb10a857780e7890d6
jrrci <jrci@outlook.ph>
2023-10-19 02:05:39 +0800

    Update remux.ps1

--- remux.ps1
+++ remux.ps1
@@ -1,7 +1,8 @@
 $desktopPath = [System.Environment]::GetFolderPath('Desktop')
 
 $inputVideo = Join-Path $desktopPath "input.ts"
+$outputVideo = Join-Path $desktopPath "output.mp4"
 
-$ffmpegCommand = "ffmpeg -i $inputVideo -c copy output.mp4 && rm $inputVideo"
+$ffmpegCommand = "ffmpeg -i $inputVideo -c copy $outputVideo && rm $inputVideo"
 
 Invoke-Expression $ffmpegCommand
