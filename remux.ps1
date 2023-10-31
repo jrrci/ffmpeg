e4fda4111b585154f6c08b95fbf5ef554dee6c9d
jrrci <jrci@outlook.ph>
2023-10-31 18:28:07 +0800

    Add files via upload

--- remux.ps1
+++ remux.ps1
@@ -1,8 +1,16 @@
 $desktopPath = [System.Environment]::GetFolderPath('Desktop')
 
-$inputVideo = Join-Path $desktopPath "input.ts"
-$outputVideo = Join-Path $desktopPath "output.mp4"
+# Get all .ts files on the desktop
+$tsFiles = Get-ChildItem $desktopPath -Filter *.ts
 
-$ffmpegCommand = "ffmpeg -i $inputVideo -c copy $outputVideo && rm $inputVideo"
-
-Invoke-Expression $ffmpegCommand
+foreach ($tsFile in $tsFiles) {
+    $inputVideo = $tsFile.FullName
+    
+    # Extract the file name from the input path
+    $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputVideo)
+    $outputVideo = Join-Path $desktopPath "$outputFileName.mp4"
+    
+    $ffmpegCommand = "ffmpeg -i `"$inputVideo`" -c copy `"$outputVideo`" && rm `"$inputVideo`""
+    
+    Invoke-Expression $ffmpegCommand
+}
